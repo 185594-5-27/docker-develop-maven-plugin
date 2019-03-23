de55be290f2f4d4a2b84f69577a6c506fc1c809a
林泽锋 <282245889@qq.com>
2019-03-23 21:56:15 +0800

    1、基于1.0版本的插件编写了一个例子

---
+++ docker-developer-maven-plugin-demo/src/main/java/com/docker/developer/maven/plugin/demo/DockerDeveloperMavenPluginDemoApplication.java
@@ -0,0 +1,16 @@
+package com.docker.developer.maven.plugin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linzf
+ */
+@SpringBootApplication
+public class DockerDeveloperMavenPluginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDeveloperMavenPluginDemoApplication.class, args);
+    }
+
+}
